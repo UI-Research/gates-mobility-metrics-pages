d2ddacf4a3e40d238c4de85cf18da31a29d32d57
awunderground <aaronrwilliams100@gmail.com>
2020-12-14 17:30:11 -0500

    Add instructions for new iteration process

--- create_pages.R
+++ create_pages.R
@@ -3,9 +3,15 @@ library(tidyverse)
 source("R/prep_pages.R")
 source("R/render_pages.R")
 
+# Mobility Metrics applicants
+prepped01 <- prep_pages(url = "data/01_rfi-applicants.csv",
+                        output_directory = "factsheets/01_rfi-applicants/")
+
+render_pages(prepped_object = prepped01)
+
 # Greg's Adams County request from 2020-12-03
-prepped02 <- prep_pages(url = "data/02_other-communities.csv",
-                        output_directory = "boom/")
+prepped02 <- prep_pages(url = "data/02_adams-county.csv",
+                        output_directory = "factsheets/02_adams-county")
 
 render_pages(prepped_object = prepped02)
 
