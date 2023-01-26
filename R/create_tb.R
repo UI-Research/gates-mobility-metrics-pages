47d4dfdc0ebe8f24fd7d694f65d429bb444fd150
deckart2 <gdmorrison@uchicago.edu>
2023-01-26 11:58:16 -0500

    add Roxygen comments and update function syntax for create_tb, creat_tb_level2, and get_vars_info

--- R/create_tb.R
+++ R/create_tb.R
@@ -1,3 +1,23 @@
+#' Create "Summary Table" for a given metric
+#' 
+#'@param metrics_info_df (list) A list composed of 13 elements with information
+#'  about a metric. In practice, the first output from the get_vars_info function
+#'  for this argument
+#'@param dataset (data.frame) A dataframe. In practice either the data or data_sub
+#'  objects
+#'@param varname_maps (list of vectors containing strings) A list containing
+#'  four vectors. 
+#'    The first vector lists the metric name(s). 
+#'    The second contains "human-readible" names. 
+#'    The third lists the metric names and their relevant confidence interval columns. 
+#'    The four lists the metric name(s) followed by the metric name(s) concatenated with "_ci"
+#'@param tb_title_size (integer) Table title size. Default set to 18
+#'@param tb_subtitle_size (integer) Table sub-title size. Default set to 14.
+#'@param tb_font_size (integer) Table font size. Default set to 12.
+#'@param source_note_size (integer) Size of source note. Default set to 11.
+#'@param tb_width_perc (float) Table width percentage. Should be between 0 and 100.
+#'  Default to 80.
+#'@param tb_align (string) Table alignment. Default set to "left" 
 create_tb <- function(metrics_info_df, 
                       dataset, 
                       varname_maps,
@@ -6,7 +26,7 @@ create_tb <- function(metrics_info_df,
                       tb_font_size = 12, 
                       source_note_size = 11,
                       tb_width_perc = 80,
-                      tb_align = 'left'
+                      tb_align = "left"
                       ){
   
   mb_metrics <- metrics_info_df$metric_name
@@ -18,7 +38,7 @@ create_tb <- function(metrics_info_df,
   col_from <- varname_maps[1][[1]]
   col_to <- varname_maps[2][[1]]
   
-  if (str_detect(subgroup_this_var, fixed('|')) == TRUE) {
+  if (str_detect(subgroup_this_var, fixed("|")) == TRUE) {
     
     subgroup_this_var <- strsplit(subgroup_this_var, "|", fixed=TRUE)[[1]][1]
     
@@ -26,17 +46,17 @@ create_tb <- function(metrics_info_df,
   
   
   temp <- dataset %>% 
-    filter(subgroup_type == 'all', subgroup_id == subgroup_this_var)
+    filter(subgroup_type == "all", subgroup_id == subgroup_this_var)
   
   
   temp <- temp %>% 
-    select(matches(glue('{mb_vars}|{quality_var}|state_county'))) %>% 
-    select(-matches('_lb|_ub')) %>% 
+    select(matches(glue("{mb_vars}|{quality_var}|state_county"))) %>% 
+    select(-matches("_lb|_ub")) %>% 
     select(sort(tidyselect::peek_vars())) %>% 
     rename_at(vars(all_of(col_from)), function(x) col_to) 
   
   
-  if (str_detect(quality_var, '|') == FALSE){ #each variable has its own quality variable  
+  if (str_detect(quality_var, "|") == FALSE){ #each variable has its own quality variable  
     
     temp <- temp %>% 
       relocate(!!sym(quality_var), .after = last_col())  
@@ -44,29 +64,29 @@ create_tb <- function(metrics_info_df,
   } 
   
   temp %>% 
-    pivot_longer(!state_county, names_to='metrics', values_to='value') %>%   
-    pivot_wider(names_from = 'state_county', values_from = 'value') %>% 
+    pivot_longer(!state_county, names_to="metrics", values_to="value") %>%   
+    pivot_wider(names_from = "state_county", values_from = "value") %>% 
     arrange(match(metrics, col_to)) %>% 
-    mutate(metrics = gsub('.*_quality', 'Quality', metrics)) %>% 
+    mutate(metrics = gsub(".*_quality", "Quality", metrics)) %>% 
     select(metrics, everything()) %>% 
     gt(
-      rowname_col = 'metrics',
-      id = 'tb'
+      rowname_col = "metrics",
+      id = "tb"
     ) %>% 
     tab_header(
-      title = '', 
+      title = "", 
       subtitle = metrics_desp
     ) %>% 
-    tab_source_note(html(str_c('<b>Source:</b>', data_source, sep=' '))) %>% 
+    tab_source_note(html(str_c("<b>Source:</b>", data_source, sep=" "))) %>% 
     cols_align(
-      align = 'left',
-      columns = TRUE
+      align = "left",
+      columns = everything()
     ) %>% 
-    cols_align(
-      align = 'left',
-      columns = TRUE
-    ) %>% 
-    opt_align_table_header('left') %>% 
+    # cols_align(
+    #   align = "left",
+    #   columns = TRUE
+    # ) %>% 
+    opt_align_table_header("left") %>% 
     tab_options(
       heading.title.font.size = tb_title_size, 
       heading.subtitle.font.size = tb_subtitle_size, 
@@ -81,7 +101,7 @@ create_tb <- function(metrics_info_df,
         cell_fill(color = "#ececec")
       ),
       locations = cells_body(
-        columns = TRUE, 
-        rows = metrics == 'Quality')
+        columns = everything(), 
+        rows = metrics == "Quality")
     )
 }
--- R/get_vars_info.R
+++ R/get_vars_info.R
@@ -1,3 +1,18 @@
+#' Get information about a mobility metric
+#' 
+#' This function uses the name of a mobility metric to look up information
+#" about it in a the data_summary_file table and returns a list of all information
+#" about it and also its name. 
+#" 
+#" @param varname (string) The name of one of the mobility metrics
+#" @param data_summary_file (data.frame) A dataframe with variable information. 
+#"  Note that this argument is optional, and the m_info dataframe is used by default.
+#" @return A list with two named elements:
+#"  info_list (list with 13 named elements) proves information about the metric
+#"    including subgroup, data sources, and notes
+#"  mb_vars (string) is the name of the variable as listed in the metric_vars_prefix column
+
+
 get_vars_info <- function(varname, data_summary_file=m_info){
   
   info_lst <- data_summary_file %>% 
