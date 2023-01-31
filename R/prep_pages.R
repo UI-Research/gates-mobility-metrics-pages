50984815b3df00bb90acaa3a1a4c763086079435
deckart2 <gdmorrison@uchicago.edu>
2023-01-31 12:22:56 -0500

    add roxygen comments to prep_pages and render_pages functions

--- R/prep_pages.R
+++ R/prep_pages.R
@@ -2,6 +2,31 @@ library(tidyverse)
 library(stringr)
 library(glue)
 
+#' Function to create a dataframe used to create a factsheet
+#' 
+#' This function works with the create_pages function to support the render_pages.R
+#' script. The point of the render_pages script is to create markdown files based
+#' on the index.Rmd markdown file. 
+#' 
+#' This function sets the "groundwork" for that to happen. It reads in a 
+#' dataframe and returns a different dataframe. The second dataframe is read by
+#' the render_pages function. 
+#' 
+#' 
+#' @param url (string) the location of a file showing the a county to analyze
+#'   and a set of comparison counties against which the county should be compared
+#' @param output_directory (string) a filepath showing where the finalized html
+#'   files should be written to
+#' @param state_title (boolean) Default to FALSE. The function returns a dataframe
+#'    with one column being a nested list (see below). One component of the list
+#'    is an element named "state_title." This argument is written into that element
+#'
+#' @returns run (tibble) A dataframe with three columns: 
+#'  filename - set to be "index.html" 
+#'  params - contains a list of the fips code listed in the fips and comparisons 
+#'    columns of the datasets pointed to by the url argument
+#'  dirname - location of where knitted hmtl files should be saved
+#'  
 prep_pages <- function(url, output_directory, state_title = FALSE) {
   
   # read in the applicant list 
