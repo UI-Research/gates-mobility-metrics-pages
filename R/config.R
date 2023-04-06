7b6b24760e971e3bb20bb055052413b910635738
deckart2 <gmorrison@urban.org>
2023-04-06 18:40:30 +0000

    reorder config

--- R/config.R
+++ R/config.R
@@ -1,12 +1,11 @@
 #Config script
 
 
-
+install.packages("tidyverse")
 install.packages("here")
 install.packages("gt")
 install.packages("qdapRegex")
 install.packages("tictoc")
-install.packages("tidyverse")
 install.packages("quarto")
 install.packages("tidycensus")
 install.packages("future")
