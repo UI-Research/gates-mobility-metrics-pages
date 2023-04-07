bbe63632f44c65dbd5822e0308ff0fb39b71bb88
Gabe Morrison <gmorrison@urban.org>
2023-04-07 16:38:00 +0000

    setup to write pages to s3

--- aws_cp_command.sh
+++ aws_cp_command.sh
@@ -1,5 +1,5 @@
-aws configure set default.s3.max_concurrent_requests 30
-aws s3 cp factsheets/ s3://mobility-metrics-data-pages-dev \
+sudo aws configure set default.s3.max_concurrent_requests 100
+sudo aws s3 cp factsheets/ s3://mobility-metrics-data-pages-dev \
     --recursive \
     --exclude "*" \
     --include "999_county-pages*" \
