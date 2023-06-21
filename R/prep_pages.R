41f0a91b991833224992e9b3dfbbc0cdda061a6e
awunderground <aaronrwilliams100@gmail.com>
2023-06-21 17:09:28 -0400

    Add upward mobility page

--- R/prep_pages.R
+++ R/prep_pages.R
@@ -58,7 +58,7 @@ prep_pages <- function(url, output_directory, state_title = FALSE, fake_labels =
     runs <- tibble(
       filename = "index.html",             # creates a string with output file names in the form <index>.pdf
       params = map(app_indexes, ~list(state_county = ., state_title = state_title, fake_labels = fake_labels)), 
-      dir_name = paste0(output_directory, '/', full_name_lst, "_", app_list$random_id)
+      dir_name = paste0(output_directory, '/', full_name_lst, "_", app_list$random_id, "/")
     )        # creates a nest list of parameters for each object in the index
     
   } else {
@@ -66,7 +66,7 @@ prep_pages <- function(url, output_directory, state_title = FALSE, fake_labels =
     runs <- tibble(
       filename = "index.html",             # creates a string with output file names in the form <index>.pdf
       params = map(app_indexes, ~list(state_county = ., state_title = state_title, fake_labels = fake_labels)), 
-      dir_name = map(full_name_lst, function(x) paste0(output_directory, glue('/{x}')))
+      dir_name = map(full_name_lst, function(x) paste0(output_directory, glue('/{x}/')))
     )        # creates a nest list of parameters for each object in the index
     
   }
